c4ffb86ecf72714752b50605b4e980e022b83638
Rinkesh Rana <rinkeshr007@gmail.com>
2022-09-22 17:11:08 +0530

    moved json folder

--- pages/api/courses.js
+++ pages/api/courses.js
@@ -3,29 +3,25 @@ import * as fs from "fs";
 export default function handler(req, res) {
   const jsonDirectory = path.join(process.cwd(), "json");
 
-  fs.readFile(
-    jsonDirectory + "./pages/api/json/courses.json",
-    "utf8",
-    (err, data) => {
-      if (err) {
-        console.error(err);
-        res.status(200).json("error");
-        return;
-      } else {
-        let coursesData = JSON.parse(data);
-        let courses = [];
-        if (req.query.categoryId) {
-          for (const item of coursesData.data) {
-            if (item.categoryId === req.query.categoryId) {
-              courses.push(item);
-            }
+  fs.readFile(jsonDirectory + "./json/courses.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(200).json("error");
+      return;
+    } else {
+      let coursesData = JSON.parse(data);
+      let courses = [];
+      if (req.query.categoryId) {
+        for (const item of coursesData.data) {
+          if (item.categoryId === req.query.categoryId) {
+            courses.push(item);
           }
-        } else {
-          courses = coursesData;
         }
-        res.status(200).json(courses);
-        return;
+      } else {
+        courses = coursesData;
       }
+      res.status(200).json(courses);
+      return;
     }
-  );
+  });
 }
