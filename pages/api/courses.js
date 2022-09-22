afbb32745c8ad2996cf4d0c607b708169b2357f4
Rinkesh Rana <rinkeshr007@gmail.com>
2022-09-22 16:48:17 +0530

    changed json direcotory for vercel deployment

--- pages/api/courses.js
+++ pages/api/courses.js
@@ -1,25 +1,31 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from "fs";
 export default function handler(req, res) {
-  fs.readFile("./pages/api/json/courses.json", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(200).json("error");
-      return;
-    } else {
-      let coursesData = JSON.parse(data);
-      let courses = [];
-      if (req.query.categoryId) {
-        for (const item of coursesData.data) {
-          if (item.categoryId === req.query.categoryId) {
-            courses.push(item);
-          }
-        }
+  const jsonDirectory = path.join(process.cwd(), "json");
+
+  fs.readFile(
+    jsonDirectory + "./pages/api/json/courses.json",
+    "utf8",
+    (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(200).json("error");
+        return;
       } else {
-        courses = coursesData;
+        let coursesData = JSON.parse(data);
+        let courses = [];
+        if (req.query.categoryId) {
+          for (const item of coursesData.data) {
+            if (item.categoryId === req.query.categoryId) {
+              courses.push(item);
+            }
+          }
+        } else {
+          courses = coursesData;
+        }
+        res.status(200).json(courses);
+        return;
       }
-      res.status(200).json(courses);
-      return;
     }
-  });
+  );
 }
