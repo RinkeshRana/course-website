dc573a03834d01fe0df4b7b154429927b8a63873
Rinkesh Rana <rinkeshr007@gmail.com>
2022-09-22 17:14:56 +0530

    modified api for vercel deployment

--- pages/api/courses.js
+++ pages/api/courses.js
@@ -1,27 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from "fs";
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const jsonDirectory = path.join(process.cwd(), "json");
 
-  fs.readFile(jsonDirectory + "./json/courses.json", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(200).json("error");
-      return;
-    } else {
-      let coursesData = JSON.parse(data);
-      let courses = [];
-      if (req.query.categoryId) {
-        for (const item of coursesData.data) {
-          if (item.categoryId === req.query.categoryId) {
-            courses.push(item);
-          }
-        }
-      } else {
-        courses = coursesData;
+  const fileContents = await fs.readFile(jsonDirectory + "/data.json", "utf8");
+
+  let coursesData = JSON.parse(fileContents);
+  let courses = [];
+  if (req.query.categoryId) {
+    for (const item of coursesData.data) {
+      if (item.categoryId === req.query.categoryId) {
+        courses.push(item);
       }
-      res.status(200).json(courses);
-      return;
     }
-  });
+  } else {
+    courses = coursesData;
+  }
+  res.status(200).json(courses);
+  return;
 }
